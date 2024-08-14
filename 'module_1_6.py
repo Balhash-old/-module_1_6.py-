34743492343646c4f7821729e5711b42fdace38e
Balhash-old <tel-alex@mail.ru>
2024-08-15 00:04:29 +0300

    Add files via upload

---
+++ 'module_1_6.py
@@ -0,0 +1,16 @@
+my_dict={'Alexfndr': 1965 ,'Irina': 1965 , 'Zhenya' : 1989 , 'Vasilisa' :2017 , 'Xenia' : 2021}
+print(my_dict)
+print(my_dict['Zhenya'])
+(my_dict)['Artem']= 1987
+print(my_dict)
+my_dict.update({'Slava': 2015 , 'Nadia' : 1999})
+print(my_dict)
+del my_dict['Slava']
+#print(my_dict['Slava'])
+print(my_dict)
+my_set={1, 1, 2, 3, 2, 'key', 5, 'stop' }
+print(my_set)
+my_set.update([4,8])
+print(my_set)
+my_set.discard('stop')
+print(my_set)
